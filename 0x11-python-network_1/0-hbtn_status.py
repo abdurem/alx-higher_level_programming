f150f6f39b72c59eb9fe0bed5cb1dccc45788e3c
abdurem <76563178+abdurem@users.noreply.github.com>
2022-09-23 13:44:19 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ import urllib.request
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
-        html = response.read().strip+'$'
+        html = response.read().strip()+'$'
         print("Body response:$")
         print("-\t type:", type(html))
         print("-\t content:", html)
