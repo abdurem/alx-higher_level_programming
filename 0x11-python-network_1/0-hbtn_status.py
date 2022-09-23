3e0bf0a683a301756a1c77040f5b5ed9cbcc1484
abdurem <76563178+abdurem@users.noreply.github.com>
2022-09-23 13:51:56 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         html = response.read()
-        print("Body response:$")
-        print("-\t type:", str(type(html))+"$")
-        print("-\t content:", str(html)+"$")
-        print("-\t utf8 content:", str(html.decode("utf-8"))+"$")
+        print("Body response:")
+        print("-\t type:", type(html))
+        print("-\t content:", html)
+        print("-\t utf8 content:", html.decode("utf-8"))
